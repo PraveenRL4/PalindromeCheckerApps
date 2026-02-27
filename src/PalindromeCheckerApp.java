3aca9514118a5ee59a1ee2b2eb35a1e9fc5527ff
PraveenRL4 <rlpraveen4@gmail.com>
2026-02-27 11:01:37 +0530

    UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,69 +1,12 @@
 import java.util.Scanner;
 
+
 public class PalindromeCheckerApp {
 
-    // Node class for Singly Linked List
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    // Method to check palindrome
-    public static boolean isPalindrome(String input) {
-
-        if (input == null || input.length() == 0) {
-            return true;
-        }
-
-        // Step 1: Convert string to linked list
-        Node head = new Node(input.charAt(0));
-        Node current = head;
-
-        for (int i = 1; i < input.length(); i++) {
-            current.next = new Node(input.charAt(i));
-            current = current.next;
-        }
-
-        // Step 2: Find middle using fast & slow pointer
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Step 3: Reverse second half
-        Node prev = null;
-        Node temp = slow;
-
-        while (temp != null) {
-            Node nextNode = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = nextNode;
-        }
-
-        // Step 4: Compare first half and reversed second half
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
+    /**
+     * Application entry point for UC9.
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -71,10 +14,25 @@ public class PalindromeCheckerApp {
         System.out.print("Input : ");
         String input = sc.nextLine();
 
-        boolean result = isPalindrome(input);
+        boolean result = check(input, 0, input.length() - 1);
 
         System.out.println("Is Palindrome? : " + result);
 
         sc.close();
+    }
+    private static boolean check(String s, int start, int end) {
+
+        // Base Condition: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call (move inward)
+        return check(s, start + 1, end - 1);
     }
 }
